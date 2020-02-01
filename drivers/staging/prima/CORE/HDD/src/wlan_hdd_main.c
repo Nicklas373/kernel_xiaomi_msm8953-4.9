bc44a8fe45490f814d07e5b64b2170ec580e816c
Albert I <kras@raphielgang.org>
2020-02-01 21:36:26 +0800

    prima: Remove pointless null checks
    Code quality of prima sucks, hence I'm fixing it the lazy way.
    
    Signed-off-by: Albert I <kras@raphielgang.org>

--- drivers/staging/prima/CORE/HDD/src/wlan_hdd_main.c
+++ drivers/staging/prima/CORE/HDD/src/wlan_hdd_main.c
@@ -8590,10 +8590,7 @@ void hdd_full_pwr_cbk(void *callbackContext, eHalStatus status)
    hdd_context_t *pHddCtx = (hdd_context_t*)callbackContext;
 
    hddLog(VOS_TRACE_LEVEL_INFO_HIGH,"HDD full Power callback status = %d", status);
-   if(&pHddCtx->full_pwr_comp_var)
-   {
-      complete(&pHddCtx->full_pwr_comp_var);
-   }
+   complete(&pHddCtx->full_pwr_comp_var);
 }
 
 #ifdef WLAN_FEATURE_RMC
