85236a8a4eb06bee1fcd494df79e30f6000215f2
Yuri Konotopov <ykonotopov@gnome.org>
2020-04-28 13:02:05 +0400

    techpack: audio: forward port runtime q6 avs version detection from 3.18
    [kras: update changes to match latest techpack/audio state]
    Signed-off-by: Albert I <kras@raphielgang.org>

--- techpack/audio/asoc/msm-pcm-q6-v2.c
+++ techpack/audio/asoc/msm-pcm-q6-v2.c
@@ -387,7 +387,10 @@ static int msm_pcm_playback_prepare(struct snd_pcm_substream *substream)
 			return -ENOMEM;
 		}
 	} else {
-		if (q6core_get_avcs_api_version_per_service(
+		if (q6core_get_avs_version() < Q6_SUBSYS_AVS2_8)
+			ret = q6asm_open_write_v3(prtd->audio_client,
+				fmt_type, bits_per_sample);
+		else if (q6core_get_avcs_api_version_per_service(
 				APRV2_IDS_SERVICE_ID_ADSP_ASM_V) >=
 				ADSP_ASM_API_VERSION_V2)
 			ret = q6asm_open_write_v5(prtd->audio_client,
@@ -435,7 +438,13 @@ static int msm_pcm_playback_prepare(struct snd_pcm_substream *substream)
 			prtd->channel_map, bits_per_sample);
 	} else {
 
-		if (q6core_get_avcs_api_version_per_service(
+		if (q6core_get_avs_version() < Q6_SUBSYS_AVS2_8) {
+			ret = q6asm_media_format_block_multi_ch_pcm_v3(
+				prtd->audio_client, runtime->rate,
+				runtime->channels, !prtd->set_channel_map,
+				prtd->channel_map, bits_per_sample,
+				sample_word_size);
+		} else if (q6core_get_avcs_api_version_per_service(
 				APRV2_IDS_SERVICE_ID_ADSP_ASM_V) >=
 				ADSP_ASM_API_VERSION_V2) {
 
@@ -511,7 +520,11 @@ static int msm_pcm_capture_prepare(struct snd_pcm_substream *substream)
 				__func__, params_channels(params),
 				prtd->audio_client->perf_mode);
 
-		if (q6core_get_avcs_api_version_per_service(
+		if (q6core_get_avs_version() < Q6_SUBSYS_AVS2_8)
+			ret = q6asm_open_read_v3(prtd->audio_client,
+				FORMAT_LINEAR_PCM,
+				bits_per_sample);
+		else if (q6core_get_avcs_api_version_per_service(
 				APRV2_IDS_SERVICE_ID_ADSP_ASM_V) >=
 				ADSP_ASM_API_VERSION_V2)
 			ret = q6asm_open_read_v5(prtd->audio_client,
@@ -588,7 +601,14 @@ static int msm_pcm_capture_prepare(struct snd_pcm_substream *substream)
 			__func__, prtd->samp_rate, prtd->channel_mode,
 			bits_per_sample, sample_word_size);
 
-	if (q6core_get_avcs_api_version_per_service(
+	if (q6core_get_avs_version() < Q6_SUBSYS_AVS2_8)
+		ret = q6asm_enc_cfg_blk_pcm_format_support_v3(
+						prtd->audio_client,
+						prtd->samp_rate,
+						prtd->channel_mode,
+						bits_per_sample,
+						sample_word_size);
+	else if (q6core_get_avcs_api_version_per_service(
 			APRV2_IDS_SERVICE_ID_ADSP_ASM_V) >=
 			ADSP_ASM_API_VERSION_V2)
 		ret = q6asm_enc_cfg_blk_pcm_format_support_v5(
